08d42dc489505d8ed40f5eee8d4bce35919388f6
prtham312 <pb3488@srmist.edu.in>
2025-05-07 11:02:36 +0530

    void function in TS

--- basics2.ts
+++ basics2.ts
@@ -3,3 +3,10 @@ function getTime() : number {
     return new Date().getFullYear();
 }
 console.log(getTime())
+
+//void function 
+// does not return anything
+function sayHello() : void {
+    console.log("kaise ho bhai log")
+}
+sayHello();
